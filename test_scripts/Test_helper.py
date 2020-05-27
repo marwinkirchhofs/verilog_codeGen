74f8c8e4237d994d9494fbf1d7d2e6c04c18a5b9
Marwin Kirchhofs <m.kirchhofs@gmx.de>
2020-05-27 23:45:12 +0200

    development files removed

--- test_scripts/Test_helper.py
+++
@@ -1,8 +0,0 @@
-
-
-import sys, os 
-
-def add_srcPath():
-    s_thisPath = os.path.abspath(__file__)
-    l_srcPath = s_thisPath.split("/")[:-2] + ["src"]
-    sys.path.append( "/".join(l_srcPath) )
